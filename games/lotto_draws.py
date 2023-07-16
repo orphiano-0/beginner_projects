e432263dd25d225c2f55a98d14225d061ffeeef8
Gabriel Orphiano <gab.orphiano@gmail.com>
2023-07-16 11:28:56 +0800

    Create a feature which indicate if the user won.

--- games/lotto_draws.py
+++ games/lotto_draws.py
@@ -1,17 +1,29 @@
-# EZ2 is a 2D lotto that selects two number from field of 1 to 31.
-# create a 2d lotto that randomly pick two numbers in the drawbox
-    # apply arguments, decorators, and functions
-
 from random import randint
+
 def repeat(func):
     def wrapper(*args, **kwargs):
-        func(*args, **kwargs)
-        func(*args, **kwargs)
+        return [func(*args, **kwargs), func(*args, **kwargs)]
     return wrapper
 
-@repeat
 def lotto_number(start, end):
     number = randint(start, end)
     print(f"The number drawn: {number}")
+    return number
 
-lotto_number(1, 31)
+@repeat
+def play_lotto(start, end):
+    drawn_number = lotto_number(start, end)
+    return drawn_number
+
+# function that determine if the users number won.
+def check_winning_numbers(numbers, drawn_numbers):
+    if numbers[0] == drawn_numbers[0] and numbers[1] == drawn_numbers[1]:
+        print("Congratulations! You've won 50000 pesos!")
+    else:
+        print("Better luck next time!")
+
+user_numbers = [18, 23]
+drawn_numbers = play_lotto(1, 31)
+
+# Check for winning numbers
+check_winning_numbers(user_numbers, drawn_numbers)
